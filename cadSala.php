d2d6920654ea4e720f65bd81c209820195d14484
Wolaci <wolacijose8@gmail.com>
2019-02-21 15:30:06 -0300

    Criação do formulário para o cadastro de salas salvando do bd

---
+++ cadSala.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Cadastro de salas</title>
+</head>
+<body>
+ <form method="POST" action="addSala.php">
+ 	<label>Nome da sala: </label>
+ 	<input type="text" name="nome">
+ 	<label>Bloco: </label>
+ 	<input type="text" name="bloco">
+ 	<input type="submit" value="cadastrar">
+ </form>
+	
+</body>
+</html>
---
+++ conexao.php
@@ -0,0 +1,6 @@
+<?php 
+	$username = "pep2";
+	$password = "pep2";
+	$conn = new PDO('mysql:host=localhost;porta=3306;dbname=pep2_reservas',$username,$password);
+
+ ?>
