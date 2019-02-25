493bdc036a2c20e2df239833b9cc8617d2ca8eac
Luigimartins <luigi.martins.355@gmail.com>
2019-02-25 17:10:00 -0300

    #8 - alteração no css

--- cadSala.php
+++ cadSala.php
@@ -7,23 +7,30 @@
 		div.formgroup{
 			margin-bottom: 10px;
 		}
+		fieldset{
+			width: 500px;
+			margin: auto;
+		}
 	</style>
 </head>
 <body>
- <form method="POST" action="addSala.php">
-	<fieldset>
-		<legend>Cadastrar salas</legend>
-		<div class="formgroup">
-		 	<label>Nome da sala: </label>
-		 	<input type="text" name="nome">		
-		</div>
-		<div class="formgroup">
-		 	<label>Bloco: </label>
-		 	<input type="text" name="bloco">
-		</div>
-	 	<input type="submit" value="Cadastrar">
-	</fieldset>
- </form>
+	<h1>Cadastro de Salas</h1>
+	<p>As salas devem serem cadastradas com um nome e o bloco onde se encontram.</p>
+	<hr>
+	<form method="POST" action="addSala.php">
+		<fieldset>
+			<legend>Cadastrar salas</legend>
+			<div class="formgroup">
+			 	<label>Nome da sala: </label><br>
+			 	<input type="text" name="nome">		
+			</div>
+			<div class="formgroup">
+			 	<label>Bloco: </label><br>
+			 	<input type="text" name="bloco">
+			</div>
+		 	<input type="submit" value="Cadastrar">
+		</fieldset>
+	</form>
 	
 </body>
 </html>
