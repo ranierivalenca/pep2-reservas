67367283fd9e52570e27133a816203d537d7d046
= <=>
2019-02-25 17:19:43 -0300

    #12 tela de autenticação com formulario

--- action_login.php
+++ action_login.php
@@ -1,18 +1,18 @@
 <?php https://github.com/ranierivalenca/pep2-reservas.git
 
-if (isset( $_POST['login']) && isset($_POST['senha'])){
+if (isset( $_POST['login']) && isset($_POST['senha'])){ //verifica se as variaveis estão setadas
 
 	$login = $_POST['login'];
 	$senha = $_POST['senha'];
 	include "configs/init.php";
 
-	$stmt = $conn -> prepare("SELECT * FROM users WHERE username = :login AND senha = :senha");
-	$stmt -> bindParam(':login' ,$login);
+	$stmt = $conn -> prepare("SELECT * FROM users WHERE username = :login AND senha = :senha"); 
+	$stmt -> bindParam(':login' ,$login); //seleciona o login e senha correspondente ao user e senha
 	$stmt -> bindParam(':senha',md5($senha));
 	$stmt -> execute();
 
-	if ($stmt -> rowCount() >= 1){
-		$result = $stmt -> fetch();
+	if ($stmt -> rowCount() >= 1){ //se a consulta for bem sucessida ele loga o user com, o login "id " and "username"
+ 		$result = $stmt -> fetch();
 		session_start();
 		$_SESSION['username'] = $login;
 		$_SESSION['id_user'] = $result['id'];
@@ -20,7 +20,7 @@ if (isset( $_POST['login']) && isset($_POST['senha'])){
 
 	}else{
 
-		echo "não logado";
+		echo "não logado"; //teste do não logado para substituir posteriormente
 
 	}
 }
--- login-user.php
+++ login-user.php
@@ -1,34 +1,35 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-</head>
-<style type="text/css">
-	@import url('https://fonts.googleapis.com/css?family=Laila');
-	* {
-		font-family: 'Laila', serif;
-	}
-	fieldset{
-		margin-left: 40em;
-		margin-right: 40em;
-		margin-top: 10em;
-	}
-</style>
-<body>
-	<h1> Autenticação </h1>
-	<p> Tela de Autenticação do usuário </p>
-<hr>
-	<fieldset>
+	<head>
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="style-login-user.css">
+	</head>
+		<style type="text/css">
+			@import url('https://fonts.googleapis.com/css?family=Laila');
+			* {
+				font-family: 'Laila', serif; 
+			}
+			fieldset{
+				margin-left: 40em;
+				margin-right: 40em;
+				margin-top: 10em;
+			}
+		</style>
+	<body>
+		<h1> Autenticação </h1>
+		<p> Tela de Autenticação do usuário </p>
+		<hr>
+		<fieldset>
 		<legend> Autenticação </legend>
 			<div id="login" align="center">
-			<form action="action_login.php" method="post">
-				<label>Nome do usuario</label></br>
-				<input type="text" name="login" required=""> <br><br>
-				<label>Senha</label></br>
-				<input type="password" name="senha" required=""><br><br>
-				<input type="submit" name="enviar" value="Enviar">
-			</form>
+				<form action="action_login.php" method="post"> <!-- formulario simples para envio, e autenticação -->
+					<label>Nome do usuario</label></br>
+					<input type="text" name="login" class="login" required=""> <br><br>
+					<label>Senha</label></br>
+					<input type="password" name="senha" required=""><br><br>
+					<input type="submit" name="enviar" class="enviar" value="Enviar">
+				</form>
 		</div>
-	</fieldset>
-</body>
+		</fieldset>
+	</body>
 </html>
