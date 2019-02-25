edad210154aeff33030c498700d092e0c9beefcf
Leonardo-AD <leonardo.alves779@gmail.com>
2019-02-25 17:54:57 -0300

    #3 integração html - php

--- register.php
+++ register.php
@@ -3,21 +3,19 @@ include "configs/init.php";
 
 
 
-$nome 	     = $_POST['nome'];
-$username    = $_POST['username'];
-$email 	     = $_POST['email'];
-$senha	     = $_POST['senha'];
+$nome 	     = strip_tags($_POST['nome']);
+$username    = strip_tags($_POST['username']);
+$email 	     = strip_tags($_POST['email']);
+$senha	     = md5(strip_tags($_POST['senha']));
 
 
 //função responsável por cadastrar os usurios
 function cadastro_de_user($nome,$username,$email,$senha){
 	global $conn;
-	
-	if(ver_user_exis($username,$email)){ 
-
+	if(ver_user_exis($username,$email)){
 		$stmt = $conn->prepare("INSERT INTO users(name,username,email,senha,is_admin) VALUES (?,?,?,?,?)");
-		$stmt -> execute([$nome,$username,$email,md5($senha),0]);
-		echo "SEJA BEM VINDO";
+		$stmt -> execute([$nome,$username,$email,$senha,0]);
+		//echo "SEJA BEM VINDO";
 	}
 
 
