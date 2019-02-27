9284eb3c926f8d3c9ec75fb12bb8ad9b9ddc9562
josecarlosmonteiro <1monteirocarlos@gmail.com>
2019-02-26 21:53:40 -0300

    correção da consulta de salas disponíveis

--- php/consultar-salas.php
+++ php/consultar-salas.php
@@ -7,34 +7,13 @@
 	$inicio = $dia . " " . $_POST['inicio'];
 	//horário de término da reserva
 	$termino = $dia . " " . $_POST['termino'];
-	//array para guardar salas disponíveis
-	$disponiveis = [];
-
-	//query guardando todas as salas cadastradas
-	$salas = $pdo->prepare("SELECT * FROM salas");
-	$salas->execute();
+	
+	//query para seleção das salas onde o id não consta na tabela de reservas ou a data e horário não entram em conflito com
+	//as especificadas pelo usuário
+	$salas = $pdo->prepare("SELECT salas.id, salas.nome, salas.bloco FROM salas JOIN reservas ON salas.id != reservas.sala_id OR (? NOT BETWEEN reservas.hora_ini AND reservas.hora_fim AND ? NOT BETWEEN reservas.hora_ini AND hora_fim) OR (? >= reservas.hora_fim OR ? <= reservas.hora_ini);");
+	$salas->execute([$inicio, $termino, $inicio, $termino]);
 	$salas = $salas->fetchAll(PDO::FETCH_ASSOC);
 
-	//query guardando todas as salas reservadas na data e horário designados
-	$reservadas = $pdo->prepare("SELECT sala_id FROM reservas WHERE ? BETWEEN hora_ini AND hora_fim OR ? BETWEEN hora_ini AND hora_fim");
-	$reservadas->execute([$inicio, $termino]);
-	$reservadas = $reservadas->fetchAll(PDO::FETCH_ASSOC);
-
-	//caso não haja reservas no dia e horário designados, todas as salas são disponíveis
-	if(count($reservadas) < 1){
-		$disponiveis = $salas;
-
-	//caso contrário é comparado o id da sala com o id da sala reservada (FK) 
-	}else{
-		for($i=0; $i < sizeof($salas); $i++){
-			for($j=0; $j < sizeof($reservadas); $j++){
-				//caso as id's sejam diferentes a sala está disponível, preenchendo um espaço na array de salas disponíveis
-				if($salas[$i]['id'] != $reservadas[$j]['sala_id']){
-					array_push($disponiveis, ['nome' => $salas[$i]['nome'], 'bloco' => $salas[$i]['bloco']]);
-				}
-			}
-		}
-	}
 ?>
 
 <h2>Salas Disponíveis</h2>
@@ -42,11 +21,13 @@
 	<tr>
 		<th>Nome</th>
 		<th>Bloco</th>
+		<th>---</th>
 	</tr>
-	<?php foreach ($disponiveis as $sala): ?>
+	<?php foreach ($salas as $sala): ?>
 		<tr>
 			<td><?= $sala['nome'] ?></td>
 			<td><?= $sala['bloco'] ?></td>
+			<td><a href="#?idSala=<?=$sala['id']?>">Reservar Sala</a></td>
 		</tr>
 	<?php endforeach; ?> 
 </table>
