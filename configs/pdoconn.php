470721e71e72fcffb3225472fbdfacd4beea8615
Murielson <jhonsnoow32@gmail.com>
2019-02-28 01:37:33 -0300

    #14 Colocando querys de listagem de salas e equipamentos

--- configs/pdoconn.php
+++ configs/pdoconn.php
@@ -1,17 +1,45 @@
 <?php
 
-global $conn;
+session_start();
 
-function conect($user, $senha){	
-	session_start();
+$_SESSION['user'] = "floki";
+
+// function conect($user, $pass){
+
 	try {
-		$conn = new PDO('mysql:host=localhost;dbname=pep2_reservas', $user, $senha);
-		
+
+		$conn = new PDO('mysql:host=localhost;dbname=pep2_reservas', "pep2", "pep2");
+
 	} catch (PDOException $e) {
+
 		echo "Error :". $e->getMessage();
-	}
-}
-function queryEquips(){
-$query= $conn->prepare("SELECT * FROM equips WHERE")
-}
+
+		}
+
+// }
+
+// function listClassRooms($conn){
+
+	$query=$conn->prepare("SELECT s.id, s.nome, s.bloco, r.id FROM salas s INNER JOIN reservas r ON s.id = r.sala_id");
+
+	$query->execute();
+
+	$result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+// }
+
+	$queryEquips=$conn->prepare("SELECT te.id, te.tipo, e.id, e.nome, e.type_id, r.id, r.user_id, r.sala_id, re.id, re.equip_id FROM type_equips te 
+		INNER JOIN equips e ON type.id = e.type_id 	
+		INNER JOIN reservas_equips re ON e.id = re.equip_id 		
+		INNER JOIN reservas r 
+		");
+	$queryEquips->execute([$_SESSION['user']]);
+
+	$resultEquips = $queryEquips->fetchAll(PDO::FETCH_ASSOC);
+
+	echo "<pre>";
+
+	print_r($resultEquips);
+
+	echo "</pre>";
 ?>
