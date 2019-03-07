c875a0ba2e02ea91e73979d7165009d0fc2b0d8e
silvio <silvioej@gmail.com>
2019-03-07 13:17:45 -0300

    #14 exibição da tabela de salas via ajax e implementação do layout

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
-<?php 
-require_once "configs/pdoconn.php";
-
-?>
+<?php require_once "configs/pdoconn.php"  ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -10,51 +7,46 @@ require_once "configs/pdoconn.php";
 	<title>Edite sua reserva</title>
 	<link rel="stylesheet" href="edit.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
 <!-- Div container onde todo conteúdo ficará presente	 -->
 	<div id="container">
 		<!-- Div do formulário -->
 		<div id="form-camp">
+			<form action="edit_reservas.php" method="post" id="form">	
 			<h2>Olá, <span><?= $_SESSION['user']."  =)"?></span></h2><br>
-			<h1>Edite sua <span>Reserva :</span></h1><br>	
-			<form action="edit_reservas.php" method="post">
-				<label for="salas">Salas :
-					<select name="salas" id="salas">
-						<?php foreach ($result as $id => $value):?>
-							<option value="<?= $id ?>"><?= $value['nome']?></option>
-						<?php endforeach; ?>	
-					</select>
-				</label>
-				<br><br>
-					<h2>Horários <span>Disponíveis :</span></h2><br><hr><br>	
-				<label for="1">Hora de inicio:	
+				<br>
+				<h2>Horários <span>Disponíveis :</span></h2><br><hr><br>
+				<label for="1">Data :
+					<input type="date" name="date" style="width: 130px;">
+				</label><br>
+				<label for="2">Hora de inicio:	
 					<!-- inputs de hora, seus values serão o retorno do banco de dados -->
-					<input type="time" name="hora-inicio" id="1" value="00:00" min="1:00" max="3:00" required="">
+					<input type="time" name="hora-inicio" id="2" value="00:00" min="1:00" max="3:00" required="">
 				</label>
-				<label for="2">Hora do término:
-					<input type="time" name="hora-fim" id="2" value="00:00" required="">
+				<label for="3">Hora do término:
+					<input type="time" name="hora-fim" id="3" value="00:00" required="">
 				</label><br><br>
-				<h2>Equipamentos <span> Reservados :</span></h2><br>
-				<table id="form-table">
-					<thead>
-						<tr>
-							<th>Tipos</th>
-							<th>Equipamentos</th>
-							<th>Ações</th>
-						</tr>
-					</thead>
-					<tbody>
-						<!-- <tr>
-							<td>Projetor</td>
-							<td><input type="number" min="0" name="projetor"></td>
-							<td><a href=""><i class="fas fa-trash-alt"></i></a></td>
-						</tr> -->
-					</tbody>
-				</table>
-			<!-- <a href="#"><button>voltar</button></a>  -->
-				<br><button>Atualizar <i class="fas fa-edit"></i></button>
+				<input type="submit"><br><br>
+				<h2>Salas <span> Disponíveis :</span></h2><br>
 			</form>
+			<div id="list-salas"></div>
+				<table id="form-table">
+<script>
+
+	$("#form").on('submit', function(e){
+					e.preventDefault();
+					$.ajax({
+						url: 'edit_reservas.php',
+						type: 'POST',
+						data:$(this).serialize(),
+						success: function(a){
+							$("#list-salas").html(a);
+						},
+					});
+				});
+</script>
 		</div>
 	</div>	
 </body>
