196baad6bb9bdbd1711e80110bc6e94915efd49c
silvio <silvioej@gmail.com>
2019-03-07 13:15:09 -0300

    #14 dados do formulário realocados para edit_reservas.php

--- configs/pdoconn.php
+++ configs/pdoconn.php
@@ -20,26 +20,6 @@ $_SESSION['user'] = "floki";
 
 // function listClassRooms($conn){
 
-	$query=$conn->prepare("SELECT s.id, s.nome, s.bloco, r.id FROM salas s INNER JOIN reservas r ON s.id = r.sala_id");
-
-	$query->execute();
-
-	$result = $query->fetchAll(PDO::FETCH_ASSOC);
 
 // }
-
-	$queryEquips=$conn->prepare("SELECT te.id, te.tipo, e.id, e.nome, e.type_id, r.id, r.user_id, r.sala_id, re.id, re.equip_id FROM type_equips te 
-		INNER JOIN equips e ON type.id = e.type_id 	
-		INNER JOIN reservas_equips re ON e.id = re.equip_id 		
-		INNER JOIN reservas r 
-		");
-	$queryEquips->execute([$_SESSION['user']]);
-
-	$resultEquips = $queryEquips->fetchAll(PDO::FETCH_ASSOC);
-
-	echo "<pre>";
-
-	print_r($resultEquips);
-
-	echo "</pre>";
 ?>
